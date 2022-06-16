f9bf6d9fd35090534d24778d138f3179e60d8e83
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-06-16 23:50:33 +0300

    end

--- src/fetch_pixabey.js
+++ src/fetch_pixabey.js
@@ -1,28 +1,54 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Axios from 'axios';
+
+//const axios = require('axios');
 export default class NewsApiServise {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
   }
 
-    fetchArticles() {
-        console.log(this);
-        // const options = {
-        //   headers: {
-        //     Authorization: '28074243-fd9335165c63977f864a46342',
-        //   },
-        // };
-      
-        console.log(this.searchQuery);
-        console.log(this.page)
-        const url = `https://pixabay.com/api/?key=28074243-fd9335165c63977f864a46342&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
-        return fetch(url)
-             .then(response => response.json())
-      .then(({hits}) => {
-        console.log(hits);
-        this.incrementPage();
-        return hits;
-      });
+  async fetchArticles() {
+    const url = `https://pixabay.com/api/?key=28074243-fd9335165c63977f864a46342&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+    
+    return await Axios.get(url).then((response) => {
+      console.log(response.data);
+      console.log(response.data.totalHits)
+      response.data.totalHits -= response.data.hits.length * this.page;
+      console.log(this.page)
+      localStorage.setItem("totalHits", response.data.totalHits);
+      if (response.data.totalHits < 0) {
+        Notify.info("We're sorry, but you've reached the end of search results.");
+      }
+      this.incrementPage();
+    return  response.data
+    });
   }
+  // =====================================
+  // fetchArticles() {
+  //   console.log(this);
+  //  console.log(this.searchQuery);
+  //   console.log(this.page)
+  //   const url = `https://pixabay.com/api/?key=28074243-fd9335165c63977f864a46342&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=100&page=${this.page}`;
+  //   return fetch(url)
+  //     .then(response => response.json())
+  //     .then(({ hits, totalHits }) => {
+
+  //       console.log(totalHits);
+  //       console.log(hits.length)
+  //       totalHits -= hits.length*this.page;
+  //       localStorage.setItem("totalHits", totalHits);
+  //       //Number(localStorage.getItem("totalHits"))
+  //       if (totalHits < 0 ) {
+  //         Notify.info("We're sorry, but you've reached the end of search results.");
+
+  //       }
+  //       console.log(totalHits)
+  //       this.incrementPage();
+  //       return { hits, totalHits };
+  //     })
+
+  // }
   incrementPage() {
     this.page += 1;
   }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 // 28074243-fd9335165c63977f864a46342
 import NewsApiServise from './fetch_pixabey';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 const refs = {
-    searchForm: document.querySelector(".search-form"),
-     articlesContainer: document.querySelector('.js-articles-container'),
-inputEl : document.querySelector(".input"),
-    btnEl: document.querySelector(".btn"),
-loadMoreBtn: document.querySelector('[data-action="load-more"]'),
-  loadMoreBtnSpiner: document.querySelector(".spinner-border"),
-  loadMoreBtnSpan: document.querySelector(".label")
-}
+  searchForm: document.querySelector('.search-form'),
+  articlesContainer: document.querySelector('.js-articles-container'),
+  inputEl: document.querySelector('.input'),
+  btnEl: document.querySelector('.btn'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+  loadMoreBtnSpiner: document.querySelector('.spinner-border'),
+  loadMoreBtnSpan: document.querySelector('.label'),
+  galleryEl: document.querySelector(".gallery")
+};
 
-
+let lightbox;
 const newsApiServise = new NewsApiServise();
-let searchQuery = "";
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.loadMoreBtn.addEventListener('click', localStorageChange)
 
 function showBtnLoadMore() {
   refs.loadMoreBtn.classList.remove('is-hidden');
@@ -24,10 +30,10 @@ hideBtnLoadMore();
 function hideBtnLoadMore() {
   refs.loadMoreBtn.classList.add('is-hidden');
 }
-console.log(refs.loadMoreBtnSpiner.textContent)
+
 function enableLoadMore() {
   refs.loadMoreBtn.disabled = false;
-  refs.loadMoreBtnSpan.textContent = 'Показать ещё';
+  refs.loadMoreBtnSpan.textContent = 'Load more';
   refs.loadMoreBtnSpiner.classList.add('is-hidden');
 }
 
@@ -42,45 +48,75 @@ function onSearch(ev) {
 
   newsApiServise.resetPage();
   showBtnLoadMore();
-  disableLoadeMore()
- 
-  newsApiServise.query = ev.currentTarget.elements.query.value;
-  console.log(newsApiServise.query);
-  newsApiServise.fetchArticles().then(articles => {
-    clearArticlesContainer();
-    appendArticlesMarkup(articles);
-    enableLoadMore()
-  });
+  disableLoadeMore();
 
+  newsApiServise.query = ev.currentTarget.elements.query.value;
+ 
+  newsApiServise.fetchArticles().then(({ hits, totalHits }) => {
+    if (hits.length !== 0) {
+      Notify.success(`Hooray! We found ${totalHits += 40} images.`);
+    }
+    if (hits.length === 0 ) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      ) 
+      hideBtnLoadMore();
+    } else if (totalHits <= hits.length) {
+      Notify.info("We're sorry, but you've reached the end of search results.")
+        hideBtnLoadMore();
+      };
+    clearArticlesContainer();
+    appendArticlesMarkup(hits);
+    enableLoadMore();
+  });
 }
 
 function clearArticlesContainer() {
-   refs.articlesContainer.innerHTML = '';
+  refs.articlesContainer.innerHTML = '';
 }
+let localStor = 0;
 
 function onLoadMore() {
-  disableLoadeMore()
-  
-  newsApiServise.fetchArticles().then(articles => {
-    appendArticlesMarkup(articles);
-enableLoadMore()
+   
+  disableLoadeMore();
+lightbox.refresh()
+  newsApiServise.fetchArticles().then(({ hits, totalHits }) => {
+    localStor = totalHits;
+    console.log(hits.length);
+    console.log(totalHits);
+    
+    appendArticlesMarkup(hits);
+    enableLoadMore();
+    if (totalHits < 0) {
+     hideBtnLoadMore();
+   }
   });
+ 
+}
+console.log(localStor)
+function localStorageChange() {
+  
+    
+    let local = Number(localStorage.getItem('totalHits'));
+    console.log(local);
+    localStorage.setItem('totalHits', local -= 40);
+
+
 }
 
-function appendArticlesMarkup(articles) {
-  console.log(articles);
-  refs.articlesContainer.insertAdjacentHTML(
-    'beforeend',
-    renderMarkup(articles)
-  );
+
+function appendArticlesMarkup(hits) {
+ 
+  refs.articlesContainer.insertAdjacentHTML('beforeend', renderMarkup(hits));
+   lightbox = new SimpleLightbox(".gallery div a", { captionData: "alt", captionDelay: 250 });
 }
 
 function renderMarkup(articles) {
-  console.log(articles);
+ 
   return articles
-    .map(({ webformatURL, likes, views, comments, tags, downloads  }) => {
+    .map(({ webformatURL, largeImageURL, likes, views, comments, tags, downloads }) => {
       return `<div class="photo-card">
-  <img width="300" height="200" src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <a  href="${largeImageURL}"><img width="300" height="200" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>\n${likes}
@@ -92,13 +128,13 @@ function renderMarkup(articles) {
       <b>Comments</b>\n${comments}
     </p>
     <p class="info-item">
-      <b>Downloads</b>\n${downloads }
+      <b>Downloads</b>\n${downloads}
     </p>
   </div>
 </div>`;
     })
     .join('');
+  
 }
-
 
 
