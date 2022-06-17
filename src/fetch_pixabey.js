439aebfd5f5f03d13e6073a7bb8b60549d3bb51b
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-06-17 09:52:14 +0300

    enddd

--- src/fetch_pixabey.js
+++ src/fetch_pixabey.js
@@ -1,7 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import Axios from 'axios';
 
-//const axios = require('axios');
 export default class NewsApiServise {
   constructor() {
     this.searchQuery = '';
@@ -10,45 +9,15 @@ export default class NewsApiServise {
 
   async fetchArticles() {
     const url = `https://pixabay.com/api/?key=28074243-fd9335165c63977f864a46342&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
-    
-    return await Axios.get(url).then((response) => {
-      console.log(response.data);
-      console.log(response.data.totalHits)
+
+    return await Axios.get(url).then(response => {
       response.data.totalHits -= response.data.hits.length * this.page;
-      console.log(this.page)
-      localStorage.setItem("totalHits", response.data.totalHits);
-      if (response.data.totalHits < 0) {
-        Notify.info("We're sorry, but you've reached the end of search results.");
-      }
+      localStorage.setItem('totalHits', response.data.totalHits);
       this.incrementPage();
-    return  response.data
+      return response.data;
     });
   }
-  // =====================================
-  // fetchArticles() {
-  //   console.log(this);
-  //  console.log(this.searchQuery);
-  //   console.log(this.page)
-  //   const url = `https://pixabay.com/api/?key=28074243-fd9335165c63977f864a46342&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=100&page=${this.page}`;
-  //   return fetch(url)
-  //     .then(response => response.json())
-  //     .then(({ hits, totalHits }) => {
 
-  //       console.log(totalHits);
-  //       console.log(hits.length)
-  //       totalHits -= hits.length*this.page;
-  //       localStorage.setItem("totalHits", totalHits);
-  //       //Number(localStorage.getItem("totalHits"))
-  //       if (totalHits < 0 ) {
-  //         Notify.info("We're sorry, but you've reached the end of search results.");
-
-  //       }
-  //       console.log(totalHits)
-  //       this.incrementPage();
-  //       return { hits, totalHits };
-  //     })
-
-  // }
   incrementPage() {
     this.page += 1;
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // 28074243-fd9335165c63977f864a46342
 import NewsApiServise from './fetch_pixabey';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
-
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
@@ -14,14 +13,14 @@ const refs = {
   loadMoreBtn: document.querySelector('[data-action="load-more"]'),
   loadMoreBtnSpiner: document.querySelector('.spinner-border'),
   loadMoreBtnSpan: document.querySelector('.label'),
-  galleryEl: document.querySelector(".gallery")
+  galleryEl: document.querySelector('.gallery'),
 };
 
 let lightbox;
 const newsApiServise = new NewsApiServise();
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
-refs.loadMoreBtn.addEventListener('click', localStorageChange)
+refs.loadMoreBtn.addEventListener('click', localStorageChange);
 
 function showBtnLoadMore() {
   refs.loadMoreBtn.classList.remove('is-hidden');
@@ -51,20 +50,24 @@ function onSearch(ev) {
   disableLoadeMore();
 
   newsApiServise.query = ev.currentTarget.elements.query.value;
- 
+
   newsApiServise.fetchArticles().then(({ hits, totalHits }) => {
-    if (hits.length !== 0) {
-      Notify.success(`Hooray! We found ${totalHits += 40} images.`);
+    if (totalHits >= 40) {
+      Notify.success(`Hooray! We found ${(totalHits += 40)} images.`);
     }
-    if (hits.length === 0 ) {
+    if (hits.length < 40 && hits.length > 0) {
+      Notify.success(`Hooray! We found ${hits.length} images.`);
+    }
+    if (hits.length === 0) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
-      ) 
+      );
       hideBtnLoadMore();
-    } else if (totalHits <= hits.length) {
-      Notify.info("We're sorry, but you've reached the end of search results.")
-        hideBtnLoadMore();
-      };
+    }
+    if (totalHits < hits.length) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      hideBtnLoadMore();
+    }
     clearArticlesContainer();
     appendArticlesMarkup(hits);
     enableLoadMore();
@@ -77,46 +80,44 @@ function clearArticlesContainer() {
 let localStor = 0;
 
 function onLoadMore() {
-   
   disableLoadeMore();
-lightbox.refresh()
+  lightbox.refresh();
   newsApiServise.fetchArticles().then(({ hits, totalHits }) => {
     localStor = totalHits;
-    console.log(hits.length);
-    console.log(totalHits);
-    
     appendArticlesMarkup(hits);
     enableLoadMore();
     if (totalHits < 0) {
-     hideBtnLoadMore();
-   }
+      hideBtnLoadMore();
+    }
   });
- 
 }
-console.log(localStor)
 function localStorageChange() {
-  
-    
-    let local = Number(localStorage.getItem('totalHits'));
-    console.log(local);
-    localStorage.setItem('totalHits', local -= 40);
-
-
+  let local = Number(localStorage.getItem('totalHits'));
+  localStorage.setItem('totalHits', (local -= 40));
 }
-
 
 function appendArticlesMarkup(hits) {
- 
   refs.articlesContainer.insertAdjacentHTML('beforeend', renderMarkup(hits));
-   lightbox = new SimpleLightbox(".gallery div a", { captionData: "alt", captionDelay: 250 });
+  lightbox = new SimpleLightbox('.gallery div a', {
+    captionData: 'alt',
+    captionDelay: 250,
+  });
 }
 
 function renderMarkup(articles) {
- 
   return articles
-    .map(({ webformatURL, largeImageURL, likes, views, comments, tags, downloads }) => {
-      return `<div class="photo-card">
-  <a  href="${largeImageURL}"><img width="300" height="200" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        likes,
+        views,
+        comments,
+        tags,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+  <a  href="${largeImageURL}"><img class="img" width="300" height="200" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>\n${likes}
@@ -132,9 +133,7 @@ function renderMarkup(articles) {
     </p>
   </div>
 </div>`;
-    })
+      }
+    )
     .join('');
-  
 }
-
-
